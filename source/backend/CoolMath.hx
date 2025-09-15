ebd8a314d10a0f7bee2b5bf4ba4c91a0af9399ab
Brainy0789 <bpurdy12@icloud.com>
2025-09-14 20:43:46 -0700

    lua support so i don't go insane

--- source/backend/CoolMath.hx
+++ source/backend/CoolMath.hx
@@ -2,5 +2,5 @@ package backend;
 
 class CoolMath
 {
-    static inline function randBool(weight:Float = 0.5) return Math.random() < weight;
+    public static inline function randBool(weight:Float = 0.5) return Math.random() < weight;
 }
