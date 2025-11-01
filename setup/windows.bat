2f2215e3b56bbe811696301eceffb7628b2d8c21
Brainy0789 <bpurdy12@icloud.com>
2025-11-01 11:17:57 -0700

    welp that was a dumb mistake

--- setup/windows.bat
+++ setup/windows.bat
@@ -5,8 +5,8 @@ cd ..
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
-haxelib install flixel 5.6.1
+haxelib install openfl 9.4.2
+haxelib install flixel 6.1.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
 haxelib install hscript-iris 1.1.3
